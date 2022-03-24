42fb3c12fb518c24a1f314bece4a02692cee963a
takuma123-type <72244249+takuma123-type@users.noreply.github.com>
2022-03-24 18:02:08 +0900

    props勉強

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Article from "./components/Article"
+
+function App() {
+    return ( <
+        div >
+        <
+        Article title = { '新世界一わかりやすいREACT入門基礎4' }
+        content = { '今日のトピックはpropのデータの受け渡しについて' }
+        /> <
+        Article title = { '新世界一わかりやすいREACT入門基礎5' }
+        content = { '今日のトピックはuseStateのデータの受け渡しについて' }
+        /> <
+        Article title = { '新世界一わかりやすいREACT入門基礎6' }
+        content = { '今日のトピックはuseEfectのデータの受け渡しについて' }
+        /> <
+        /div >
+    );
+}
+
+export default App;
